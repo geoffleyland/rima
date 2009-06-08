f460a6c2b52f350958488fcdca4959fcc1feb282
Geoff Leyland <geoff.leyland@incremental.co.nz>
2009-06-08 22:22:10 +0000

    rima: you can now run just one test if you want to.  This'll be handy for checking requires and coverage.

--- lua/rima-test.lua
+++ lua/rima-test.lua
@@ -1,27 +1,62 @@
 -- Copyright (c) 2009 Incremental IP Limited
 -- see license.txt for license information
 
-require("tests.series_test")
-require("tests.object")
-require("tests.proxy")
-require("tests.args")
-require("tests.types")
+require("tests.series")
 
 --------------------------------------------------------------------------------
 
-function test(show_passes)
+test_names =
+{
+  "series_test",
+  "object",
+  "proxy",
+  "args",
+  "types.undefined_t",
+  "types.number_t",
+}
+
+
+--------------------------------------------------------------------------------
+
+function test(prefix, names, show_passes, patterns)
   local T = tests.series:new("rima", show_passes)
 
-  T:run(tests.series_test.test)
-  T:run(tests.object.test)
-  T:run(tests.proxy.test)
-  T:run(tests.args.test)
-  T:run(tests.types.test)
-
+  for _, t in ipairs(names) do
+    local run = false
+    if not patterns or #patterns == 0 then
+      run = true
+    else
+      for _, p in ipairs(patterns) do
+        if t:match(p) then
+          run = true
+          break
+        end
+      end
+    end
+    
+    if run then
+      local m = require(prefix.."."..t)
+      T:run(m.test)
+    end
+  end
+  
   return T:close()
 end
 
-test(false)
+
+--------------------------------------------------------------------------------
+
+local patterns = {}
+local show_passes = false
+
+for _, v in ipairs{...} do
+  if v == "show_passes" then
+    patterns = true
+  else
+    patterns[#patterns+1] = v
+  end
+end
+
+test("tests", test_names, show_passes, patterns)
 
 -- EOF -------------------------------------------------------------------------
-
