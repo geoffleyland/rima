4d640235968ae5096c28c881348d9a3986cf706d
Geoff Leyland <geoff.leyland@incremental.co.nz>
2009-06-16 09:32:27 +0000

    rima: full coverage for the mul tests

--- lua/tests/operators/mul.lua
+++ lua/tests/operators/mul.lua
@@ -45,21 +45,24 @@ function test(show_passes)
   T:check_equal(2 * (3 * b), "2*3*b")
   T:check_equal(2 / (3 * b), "2/(3*b)")
 
-  T:check_equal(expression.eval(b / b, S), 1)
-  T:check_equal(expression.eval(b * b, S), "b^2")
-  T:check_equal(expression.eval(2 * (3 * b), S), "6*b")
-  T:check_equal(expression.eval(2 / (3 * b), S), "0.6667/b")
+  T:check_equal(rima.E(b / b, S), 1)
+  T:check_equal(rima.E(b * b, S), "b^2")
+  T:check_equal(rima.E(2 * (3 * b), S), "6*b")
+  T:check_equal(rima.E(2 / (3 * b), S), "0.6667/b")
 
-  T:check_equal(expression.eval(2 * (3 * a), S), 30)
-  T:check_equal(expression.eval(2 / (3 * a), S), 2/15)
+  T:check_equal(rima.E(2 * (3 * a), S), 30)
+  T:check_equal(rima.E(2 / (3 * a), S), 2/15)
+
+  T:check_equal(expression.dump(rima.E(2 * (b + b), S)), "*(number(4)^1, ref(b)^1)")
+  T:check_equal(expression.dump(rima.E(2 * b^5, S)), "*(number(2)^1, ref(b)^5)")
 
   T:check_equal(expression.dump(mul:eval(S, {{2, b}})), "*(ref(b)^2)")
   T:check_equal(expression.dump(mul:eval(S, {{1, b}})), "ref(b)", "checking we simplify identity")
-  T:check_equal(expression.dump(expression.eval(1 * b, S)), "ref(b)", "checking we simplify identity")
-  T:check_equal(expression.dump(expression.eval(2 * b / 2, S)), "ref(b)", "checking we simplify identity")
+  T:check_equal(expression.dump(rima.E(1 * b, S)), "ref(b)", "checking we simplify identity")
+  T:check_equal(expression.dump(rima.E(2 * b / 2, S)), "ref(b)", "checking we simplify identity")
 
   T:check_equal(mul:eval(S, {{0, S.b}}), 1, "checking we simplify 0")
-  T:check_equal(expression.eval(0 * S.b, S), 0, "checking we simplify 0")
+  T:check_equal(rima.E(0 * S.b, S), 0, "checking we simplify 0")
 
   -- Tests including add and pow are in rima.expression
 
