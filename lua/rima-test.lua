f0eff889262ea4772da6f94b622500b5ea296525
Geoff Leyland <geoff.leyland@incremental.co.nz>
2009-10-04 05:53:11 +0000

    rima: typo in the rima-test success message.

--- lua/rima-test.lua
+++ lua/rima-test.lua
@@ -17,7 +17,7 @@ for _, v in ipairs{...} do
 end
 
 if test.directory.test("rima", "tests", show_passes, patterns) then
-  io.stderr:write("All tests completed successfully")
+  io.stderr:write("All tests completed successfully\n")
   os.exit(0)
 else
   os.exit(1)
