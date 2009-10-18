30f2ee6130d2f08cb275580e54ed262de8f87fbd
Geoff Leyland <geoff.leyland@incremental.co.nz>
2009-10-18 01:12:00 +0000

    rima: updated the whiskas example to use structured data.  Almost perfect.

--- lua/examples/whiskas.lua
+++ lua/examples/whiskas.lua
@@ -33,53 +33,51 @@ language and doesn't need to know much type information in advance - if things
 work then it's ok.  However, I have worked on a type system that can be used
 to check what you're doing in advance.  It's just not very good yet.
 --]]
-local I = rima.R"I"                     -- ingredients
-local N = rima.R"N"                     -- nutrients
-local c = rima.R"c"                     -- cost of ingredients
-local n = rima.R"n"                     -- nutrients in each product
-local l = rima.R"l"                     -- limits on nutrients
-local T = rima.R"T"                     -- total amount of product we're producing
-
-local f = rima.R"f"                     -- ingredient fractions (result)
-
+local i, ingredients = rima.R"i, ingredients"
+local n, nutrients   = rima.R"n, nutrients"
+local limits         = rima.R"limits"       -- limits on nutrients
+local quantity       = rima.R"quantity"     -- quantity of pet food to make
 
 -- Set up the blending problem
+total_cost = rima.sum{["_, i"]=rima.pairs(ingredients)}(i.cost * i.quantity)
+total_quantity = rima.sum{["_, i"]=rima.pairs(ingredients)}(i.quantity)
+
+
 blending_problem = rima.formulation:new()
-blending_problem:scope().f[rima.default] = rima.positive()
-blending_problem:set_objective(rima.sum({I}, c[I] * f[I]), "minimise")
-blending_problem:add({}, rima.sum({I}, f[I]), "==", T)
-blending_problem:add({N}, rima.sum({I}, n[I][N] * f[I]), ">=", T * l[N])
+blending_problem:scope().ingredients[rima.default].quantity = rima.positive()
+
+blending_problem:set_objective(total_cost, "minimise")
+blending_problem:add({}, total_quantity, "==", quantity)
+
+blending_problem:add({n=nutrients}, rima.sum{["_, i"]=rima.pairs(ingredients)}(i.composition[n] * i.quantity), ">=", quantity * limits[n])
 
 -- The formulation can describe itself
 io.write("\nBlending Problem\n")
 blending_problem:write()
 --[[
-Output is
-
 Minimise:
-  sum(c[I]*f[I], I in I)
+  sum{_, i in pairs(ingredients)}(i.cost*i.quantity)
 Subject to:
-  sum(f[I], I in I) == T
-  sum(f[I]*n[I, N], I in I) >= T*l[N] for all N in N
+  sum{_, i in pairs(ingredients)}(i.quantity) == quantity
+  sum{_, i in pairs(ingredients)}(i.composition[n]*i.quantity) >= limits[n]*quantity for all {n in nutrients}
 --]]
 
 --[[
 The data comes AFTER the formulation.
 This is standard Lua table construction.
 --]]
-local whiskas_data = {
-  I = {"chicken", "beef", "mutton", "rice", "wheat bran", "gel"},
-  N = {"protein", "fat", "fibre", "salt"},
-  c = { 0.013, 0.008, 0.010, 0.002, 0.005, 0.001 },
-  l = { 0.08, 0.06, -0.02, -0.004 },
-  n =
-  {                   --  protein fat    fibre   salt
-    chicken           = { 0.100,  0.080, -0.001, -0.002 },
-    beef              = { 0.200,  0.100, -0.005, -0.005 },
-    mutton            = { 0.150,  0.110, -0.003, -0.007 },
-    rice              = { 0.000,  0.010, -0.100, -0.002 },
-    ["wheat bran"]    = { 0.040,  0.010, -0.150, -0.008 },
-    gel               = { 0.000,  0.000, -0.000, -0.000 },
+local whiskas_data =
+{
+  nutrients = {"protein", "fat", "fibre", "salt"},
+  limits = { protein = 0.08, fat = 0.06, fibre = -0.02, salt = -0.004 },
+  ingredients =
+  {                                                 --  protein fat    fibre   salt
+    chicken           = { cost = 0.013, composition = { 0.100,  0.080, -0.001, -0.002 } },
+    beef              = { cost = 0.008, composition = { 0.200,  0.100, -0.005, -0.005 } },
+    mutton            = { cost = 0.010, composition = { 0.150,  0.110, -0.003, -0.007 } },
+    rice              = { cost = 0.002, composition = { 0.000,  0.010, -0.100, -0.002 } },
+    ["wheat bran"]    = { cost = 0.005, composition = { 0.040,  0.010, -0.150, -0.008 } },
+    gel               = { cost = 0.001, composition = { 0.000,  0.000, -0.000, -0.000 } },
   },
 }
 
@@ -90,19 +88,19 @@ whiskas = blending_problem:instance(whiskas_data)
 local function s(problem, solver, S)
   local r = problem:solve(solver, S)
   io.write(("\n%s:\n  objective:  \t% 10.2f\n  variables:\n"):format(solver, r.objective))
-  for k, v in pairs(r.variables.f) do io.write(("    %-10s\t% 10.2f\t(% 10.2f)\n"):format(k, v.p, v.d)) end
+  for k, v in pairs(r.variables.ingredients) do io.write(("    %-10s\t% 10.2f\t(% 10.2f)\n"):format(k, v.quantity.p, v.quantity.d)) end
 end
 
 -- We can choose our solver and set any extra variables when we solve.
-s(whiskas, "clp", { T=1 })
+s(whiskas, "clp", { quantity=1 })
+
 -- cbc and lpsolve can solve integer problems
 -- Note that we're redefining f as an integer.  What there is of a type
 -- system lets this happen because integers are subsets of positive variables.
 -- You couldn't set f to a free variable (because a free variable is not a
 -- subset of a positive variable).
-
-whiskas_integer = whiskas:instance{ T = 99 }
-whiskas_integer:scope().f[rima.default] = rima.integer()
+whiskas_integer = whiskas:instance{ quantity = 99 }
+whiskas_integer:scope().ingredients[rima.default].quantity = rima.integer()
 
 s(whiskas_integer, "cbc")
 s(whiskas_integer, "lpsolve")
