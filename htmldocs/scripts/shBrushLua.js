539b6c3a84310f48fe1faa90938d076397cc9384
Geoff Leyland <geoff.leyland@incremental.co.nz>
2009-10-21 23:47:16 +0000

    rima: got a better lua brush for the syntax highlighter

--- htmldocs/scripts/shBrushLua.js
+++ htmldocs/scripts/shBrushLua.js
@@ -1,19 +1,67 @@
+/**
+ * SyntaxHighlighter
+ * http://alexgorbatchev.com/
+ *
+ * SyntaxHighlighter is donationware. If you are using it, please donate.
+ * http://alexgorbatchev.com/wiki/SyntaxHighlighter:Donate
+ *
+ * @version
+ * 2.0.320 (May 03 2009)
+ * 
+ * @copyright
+ * Copyright (C) 2004-2009 Alex Gorbatchev.
+ *
+ * @license
+ * This file is part of SyntaxHighlighter.
+ * 
+ * SyntaxHighlighter is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * SyntaxHighlighter is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with SyntaxHighlighter.  If not, see <http://www.gnu.org/copyleft/lesser.html>.
+ */
 SyntaxHighlighter.brushes.Lua = function()
 {
-	var keywords =	'break do end else elseif function if local nil not or repeat return and then until while this';
-	var funcs = 'math\\.\\w+ string\\.\\w+ os\\.\\w+ debug\\.\\w+ io\\.\\w+ error fopen dofile coroutine\\.\\w+ arg getmetatable ipairs loadfile loadlib loadstring print rawget rawset seek setmetatable assert tonumber tostring require';
+	var keywords =	'and break do else elseif  ' +
+					'end false for function if ' +
+					'in local nil not or ' +
+					'repeat return then true until while';
+
+	var functions =	'assert collectgarbage dofile error getfenv ' +
+		            'getmetatable ipairs load loadfile loadstring ' +
+					'next pairs pcall print rawequal ' +
+					'rawget rawset select setfenv setmetatable ' +
+					'tonumber tostring type unpack xpcall';
 
 	this.regexList = [
-		{ regex: new RegExp('--\\[\\[[\\s\\S]*\\]\\]--', 'gm'),		css: 'comments' },
-		{ regex: new RegExp('--[^\\[]{2}.*$', 'gm'),			    css: 'comments' },	// one line comments
-		{ regex: SyntaxHighlighter.regexLib.doubleQuotedString,     css: 'string' },    // strings
-		{ regex: SyntaxHighlighter.regexLib.singleQuotedString,     css: 'string' },    // strings
-		{ regex: new RegExp(this.getKeywords(keywords), 'gm'),		css: 'keyword' },	// keyword
-		{ regex: new RegExp(this.getKeywords(funcs), 'gm'),		    css: 'functions' },		// functions
-		{ regex: new RegExp(this.getKeywords('rima\\.\\w+'), 'gm'),		    css: 'color1' },		// functions
+		{ regex: new RegExp('[0-9][\\.0-9]*', 'gm'), css: 'constants' },                        // -- comments
+		{ regex: new RegExp('[{}=\\[\\]]', 'gm'), css: 'color1' },                        // -- comments
+		{ regex: new RegExp('--[^!\\[].*$', 'gm'), css: 'comments' },                        // -- comments
+		{ regex: new RegExp('--\\[\\[[\\s\\S]*?\\]\\]', 'gm'), css: 'comments' },         // [[...]] comments
+		{ regex: new RegExp('--\\[=\\[[\\s\\S]*?\\]=\\]', 'gm'), css: 'comments' },       // [==[.. comments
+		{ regex: new RegExp('--\\[==\\[[\\s\\S]*?\\]==\\]', 'gm'), css: 'comments' },     // [==[.. comments
+		{ regex: new RegExp('--\\[===\\[[\\s\\S]*?\\]===\\]', 'gm'), css: 'comments' },   // [===[.. comments
+		{ regex: new RegExp('--\\[====\\[[\\s\\S]*?\\]====\\]', 'gm'), css: 'comments' }, // [===[.. comments
+		{ regex: SyntaxHighlighter.regexLib.doubleQuotedString,	css: 'string' },          // double quoted strings
+		{ regex: SyntaxHighlighter.regexLib.singleQuotedString,	css: 'string' },          // single quoted strings
+		{ regex: new RegExp('\\[\\[[\\s\\S]*?\\]\\]', 'gm'), css: 'string' },             // [[...]] string
+		{ regex: new RegExp('\\[=\\[[\\s\\S]*?\\]=\\]', 'gm'), css: 'string' },           // [=[...]=] string
+		{ regex: new RegExp('\\[==\\[[\\s\\S]*?\\]==\\]', 'gm'), css: 'string' },         // [==[...]===] string
+		{ regex: new RegExp('\\[===\\[[\\s\\S]*?\\]===\\]', 'gm'), css: 'string' },       // [===[...]===] string
+		{ regex: new RegExp(this.getKeywords(functions), 'gm'),    css: 'functions bold' },
+		{ regex: new RegExp(this.getKeywords(keywords), 'gm'),     css: 'keyword' },       // keywords
+		{ regex: new RegExp(this.getKeywords('rima\\.[\\w\\.]+'), 'gm'),		    css: 'color1' }		// rima identifiers
 		];
-}
+	
+	this.forHtmlScript(SyntaxHighlighter.regexLib.scriptScriptTags);
+};
 
 SyntaxHighlighter.brushes.Lua.prototype	= new SyntaxHighlighter.Highlighter();
-SyntaxHighlighter.brushes.Lua.aliases = ['lua'];
-
+SyntaxHighlighter.brushes.Lua.aliases	= ['lua'];
