2abbe8afa270159c18049c822926f02baf553073
Geoff Leyland <geoff.leyland@incremental.co.nz>
2010-06-13 05:56:10 +0000

    rima: added a --no-strict option to rima-test.  Should integrate luacov better one day...

--- lua/rima-test.lua
+++ lua/rima-test.lua
@@ -1,20 +1,26 @@
 -- Copyright (c) 2009-2010 Incremental IP Limited
 -- see LICENSE for license information
 
-require("test.directory")
-require("test.strict")
 
 --------------------------------------------------------------------------------
 
 local patterns = {}
 local show_passes = false
+local strict = true
 
 for _, v in ipairs{...} do
-  if v == "show_passes" then
+  if v == "--show-passes" then
     show_passes = true
+  elseif v == "--no-strict" then
+    strict = false
   else
     patterns[#patterns+1] = v
   end
+end
+
+require("test.directory")
+if strict then
+  require("test.strict")
 end
 
 local passed, tests, fails = test.directory.test("rima", "tests", show_passes, patterns)
