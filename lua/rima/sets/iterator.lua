9576d3f772636144d89a3ae8d21423f4fa4c6cde
Geoff Leyland <geoff.leyland@incremental.co.nz>
2010-07-15 00:27:35 +0000

    rima: iterator is not a proxy and it doesn't have an __index - that doesn't work yet

--- lua/rima/sets/iterator.lua
+++ lua/rima/sets/iterator.lua
@@ -1,10 +1,7 @@
 -- Copyright (c) 2009-2010 Incremental IP Limited
 -- see LICENSE for license information
 
-local setmetatable = setmetatable
-
 local object = require("rima.lib.object")
-local proxy = require("rima.lib.proxy")
 local lib = require("rima.lib")
 local core = require("rima.core")
 
@@ -14,58 +11,57 @@ module(...)
 -- Iterators -------------------------------------------------------------------
 
 iterator = object:new(_M, "iterator")
-iterator.proxy_mt = setmetatable({}, iterator)
 
 function iterator:new(base, exp, key, value, set)
-  return proxy:new(object.new(self, { base=base, exp=exp, key=key, value=value, set=set }), proxy_mt)
+  return object.new(self, { base_=base, exp_=exp, key_=key, value_=value, set_=set })
 end
 
 
-function iterator:key() return proxy.O(self).key end
-function iterator:value() return proxy.O(self).value end
-function iterator:expression() return proxy.O(self).exp end
+function iterator:key() return self.key_ end
+function iterator:value() return self.value_ end
+function iterator:expression() return self.exp_ end
 
 
-function proxy_mt:__eval(S, eval)
-  self = proxy.O(self)
-  local exp = eval(self.exp, S)
-  return iterator:new(self.base, exp, self.key, self.value, self.set)
+function iterator:__eval(S, eval)
+  local exp = eval(self.exp_, S)
+  return iterator:new(self.base_, exp, self.key_, self.value_, self.set_)
 end
 
 
-function proxy_mt:__defined()
-  self = proxy.O(self)
-  return core.defined(self.exp)
+function iterator:__defined()
+  return core.defined(self.exp_)
 end
 
 
-function proxy_mt:__repr(format)
-  self = proxy.O(self)
+function iterator:__repr(format)
   if format.dump then
     return ("iterator(%s, key=%s, value=%s)"):
-      format(lib.repr(self.exp, format), lib.repr(self.key, format), lib.repr(self.value, format))
+      format(lib.repr(self.exp_, format), lib.repr(self.key_, format), lib.repr(self.value_, format))
   else
-    return lib.repr(self.exp, format)
+    return lib.repr(self.exp_, format)
   end
 end
-proxy_mt.__tostring = lib.__tostring
+iterator.__tostring = lib.__tostring
 
 
 -- Operators -------------------------------------------------------------------
 
 local function extract(a)
-  if iterator:isa(a) then return proxy.O(a).value end
+  if iterator:isa(a) then return a.value_ end
   return a
 end
 
-function proxy_mt.__add(a, b) return extract(a) + extract(b) end
-function proxy_mt.__sub(a, b) return extract(a) - extract(b) end
-function proxy_mt.__mul(a, b) return extract(a) * extract(b) end
-function proxy_mt.__div(a, b) return extract(a) / extract(b) end
-function proxy_mt.__pow(a, b) return extract(a) ^ extract(b) end
+function iterator.__add(a, b) return extract(a) + extract(b) end
+function iterator.__sub(a, b) return extract(a) - extract(b) end
+function iterator.__mul(a, b) return extract(a) * extract(b) end
+function iterator.__div(a, b) return extract(a) / extract(b) end
+function iterator.__pow(a, b) return extract(a) ^ extract(b) end
 
-function proxy_mt:__unm() return -proxy.O(self).value end
-function proxy_mt:__index(i) return proxy.O(self).value[i] end
+function iterator:__unm() return -self.value_ end
+
+-- Iterators don't have an __index, because indexing is more complex than
+-- indexing the iterator's value - there might be default values to deal with.
+-- The rewrite might fix this.
 
 
 -- EOF -------------------------------------------------------------------------
