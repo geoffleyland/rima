3b96d5e546bd9d3f0a9a4d5a0627ec82f22b38bb
Geoff Leyland <geoff.leyland@incremental.co.nz>
2010-08-12 01:57:22 +0000

    rima: the test selection logic in tests.directory makes more sense

--- lua/test/directory.lua
+++ lua/test/directory.lua
@@ -13,28 +13,25 @@ module(...)
 local function run_one(T, path, options, patterns)
   path = path:gsub("/", "."):gsub("%.lua$", "")
 
-  local to_run = 1
-  local run = 0
-  if not patterns or #patterns == 0 then
-    to_run = 0
-  else
+  local run = true
+  if patterns and #patterns > 0 then
+    if patterns[1]:sub(1,1) ~= "-" then
+      run = false
+    end
     for _, p in ipairs(patterns) do
       if p:sub(1,1) == "-" then
-        to_run = 0
         if path:match(p:sub(2)) then
-          run = -1
-          break
+          run = false
         end
       else
         if path:match(p) then
-          run = 1
-          break
+          run = true
         end
       end
     end
   end
 
-  if run >= to_run then
+  if run then
     local m = require(path)
     T:run(m.test)
   end
