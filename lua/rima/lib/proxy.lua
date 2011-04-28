d5c5dc2341053065d3042bec0b99f39c06b17556
Geoff Leyland <geoff.leyland@incremental.co.nz>
2011-04-28 15:23:44 +1200

    simplified proxies

--- lua/rima/lib/proxy.lua
+++ lua/rima/lib/proxy.lua
@@ -8,28 +8,17 @@ module(...)
 -- Types -----------------------------------------------------------------------
 
 local proxy = _M
-proxy._typename = "proxy"
 local objects = setmetatable({}, { __mode = "k" })
-local proxies = setmetatable({}, { __mode = "v" })
 
-function proxy:new(o, metatable, typename)
-  metatable = metatable or {}
-  metatable.__is_proxy = true
-  if typename then metatable.__typename = typename end
-  local proxy = setmetatable({}, metatable)
+function proxy:new(o, class)
+  local proxy = setmetatable({}, class)
   objects[proxy] = o
-  proxies[o] = proxy
   return proxy
 end
 
 
 function proxy.O(p)
   return objects[p] or p
-end
-
-
-function proxy.P(o)
-  return proxies[o] or o
 end
 
 
--- lua/tests/lib/proxy.lua
+++ lua/tests/lib/proxy.lua
@@ -12,18 +12,16 @@ module(...)
 function test(options)
   local T = series:new(_M, options)
 
-  local mt = {}
+  local my_type = object:new_class({}, "my_type")
 
-  local p = proxy:new({}, mt, "my_type")
-  T:test(object.isa(mt, p), "isa(mt, p)")
+  local p = proxy:new({}, my_type)
+  T:test(my_type:isa(p), "my_type:isa(p)")
   T:check_equal(object.type(p), "my_type", "type(proxy) == 'my_type'")
   
   local o = {}
-  local p = proxy:new(o, mt, "o")
+  local p = proxy:new(o, my_type)
   T:check_equal(proxy.O(p), o)
   T:check_equal(proxy.O(o), o)
-  T:check_equal(proxy.P(o), p)
-  T:check_equal(proxy.P(p), p)
 
   return T:close()
 end
