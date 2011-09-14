2fbe254db0af8d2dccf9c381f5036be5f852df68
Geoff Leyland <geoff.leyland@incremental.co.nz>
2011-09-14 14:42:25 +1200

    moved some of add's tests into expressions

--- lua/tests/operators/add.lua
+++ lua/tests/operators/add.lua
@@ -4,7 +4,6 @@
 local add = require("rima.operators.add")
 
 local series = require("test.series")
-local expression_tester = require("test.expression_tester")
 local object = require("rima.lib.object")
 local lib = require("rima.lib")
 local core = require("rima.core")
@@ -50,27 +49,6 @@ function test(options)
 
   T:check_equal(OE({{1, a}}, S), 5)
   T:check_equal(OE({{1, a}, {2, a}}, S), 15)
-
-  local U = expression_tester(T, "a, b, c", { a = 5 })  
-
-  U{ "b - b", S="b - b", D='+(1*index(address{"b"}), -1*index(address{"b"}))', ES=0 }
-  U{ "b + b", S="b + b", D='+(1*index(address{"b"}), 1*index(address{"b"}))', ES="2*b", ED='+(2*index(address{"b"}))' }
-
-  U{ "2 + b + 3", S="2 + b + 3", D='+(1*+(1*2, 1*index(address{"b"})), 1*3)', ES="5 + b" }
-  U{ "2 - (3 + b)", S="2 - (3 + b)", D='+(1*2, -1*+(1*3, 1*index(address{"b"})))', ES="-1 - b" }
-
-  U{ "2 + a + 3", S="2 + a + 3", D='+(1*+(1*2, 1*index(address{"a"})), 1*3)', ES=10 }
-  U{ "2 - (3 + a)", S="2 - (3 + a)", D='+(1*2, -1*+(1*3, 1*index(address{"a"})))', ES=-6 }
-  U{ "-a", S="-a", D='+(-1*index(address{"a"}))', ES=-5 }
-
-  -- Tests with mul
-  U{ "2 + 3*b", S="2 + 3*b", D='+(1*2, 1**(3^1, index(address{"b"})^1))', ED='+(1*2, 3*index(address{"b"}))' }
-  U{ "2 + b*c", S="2 + b*c", D='+(1*2, 1**(index(address{"b"})^1, index(address{"c"})^1))', ED='+(1*2, 1**(index(address{"b"})^1, index(address{"c"})^1))' }
-  U{ "2 + 5*b*c", S="2 + 5*b*c", D='+(1*2, 1**(*(5^1, index(address{"b"})^1)^1, index(address{"c"})^1))', ED='+(1*2, 5**(index(address{"b"})^1, index(address{"c"})^1))' }
-
-  -- Check we simplify the identity (+0)
-  U{ "1 + 2*b - 1", S="1 + 2*b - 1", ED='+(2*index(address{"b"}))' }
-  U{ "1 + b - 1", S="1 + b - 1", ED='index(address{"b"})' }
 
   return T:close()
 end
